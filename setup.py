3aee9c378296d837ae497529d334bc297edf6556
varajala <valtteri.rajalainen@gmail.com>
2021-11-18 13:24:32 +0200

    Added initial project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='templateman',
+    version='0.0.1a',
+    author='Valtteri Rajalainen',
+    python_requires='>=3.7',
+    packages=find_packages(),
+)
