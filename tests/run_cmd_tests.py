0d1fbe370c39b44d01c354ef369f7c712ef26976
varajala <valtteri.rajalainen@gmail.com>
2021-11-20 13:45:30 +0200

    Added tests for argument passing when running scripts

--- tests/run_cmd_tests.py
+++ tests/run_cmd_tests.py
@@ -5,6 +5,7 @@ import templateman.cli as cli
 
 
 path_opened = ''
+script_text = ''
 
 
 class DummyFile:
@@ -15,7 +16,7 @@ class DummyFile:
         pass
     
     def read(self):
-        return "print('module executed')\n"
+        return script_text
 
 
 def dummy_open(path: str, mode='r', *args, **kwargs) -> DummyFile:
@@ -24,10 +25,19 @@ def dummy_open(path: str, mode='r', *args, **kwargs) -> DummyFile:
     return DummyFile()
 
 
+@microtest.reset
+def on_reset():
+    global path_opened
+    path_opened = ''
+    script_text = ''
+
+
 @microtest.test
-def test_running_scripts():
+def test_running_uninstalled_scripts():
+    global script_text
     dummy_builtins = cli.__builtins__.copy()
     dummy_builtins['open'] = dummy_open
+    script_text = "print('module executed')\n"
     
     with io.StringIO() as stream:
         with contextlib.redirect_stdout(stream):
@@ -36,6 +46,26 @@ def test_running_scripts():
             
             output = stream.getvalue()
             assert 'module executed' in output
+            assert 'script.py' in path_opened
+
+
+@microtest.test
+def test_argument_passing():
+    global script_text
+    dummy_builtins = cli.__builtins__.copy()
+    dummy_builtins['open'] = dummy_open
+    script_text = '\n'.join([
+        'import templateman',
+        "print(f\"name = {templateman.template_info.get('name')}\")",
+    ])
+    
+    with io.StringIO() as stream:
+        with contextlib.redirect_stdout(stream):
+            with microtest.patch(cli, __builtins__ = dummy_builtins):
+                cli.run(['script.py', '--name', 'NAME'])
+            
+            output = stream.getvalue()
+            assert 'name = NAME' in output
             assert 'script.py' in path_opened
 
 
